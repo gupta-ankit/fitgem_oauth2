ee278ca028e89dcb0d5056f5f31a717ca23b6459
David Armour <15181647+dav-armour@users.noreply.github.com>
2020-03-21 13:52:49 +1100

    Add revoke token method to client

--- lib/fitgem_oauth2/client.rb
+++ lib/fitgem_oauth2/client.rb
@@ -51,6 +51,16 @@ module FitgemOauth2
       JSON.parse(response.body)
     end
 
+    def revoke_token(token)
+      response = connection.post('/oauth2/revoke') do |request|
+        encoded = Base64.strict_encode64("#{@client_id}:#{@client_secret}")
+        request.headers['Authorization'] = "Basic #{encoded}"
+        request.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        request.params['token'] = token
+      end
+      JSON.parse(response.body)
+    end
+
     def get_call(url)
       url = "#{API_VERSION}/#{url}"
       response = connection.get(url) { |request| set_headers(request) }
